ac5f3bd711b8a9387401efefb1c72a728b3ee000
Samuel Otton <ottonsam@gmail.com>
2024-05-16 12:36:51 -0300

    feat: commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-1">
+      <h1 className="text-3xl font-bold underline mx-auto">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
