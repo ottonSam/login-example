187525e86fe149dc305c8725eed81d34d5bcec85
Samuel Otton <ottonsam@gmail.com>
2024-05-16 22:32:27 -0300

    feat: sistema de autenticação

---
+++ src/types/User.ts
@@ -0,0 +1,21 @@
+export type User = {
+  id: string;
+  avatar: {
+    id: number;
+    image_high_url: string;
+    image_medium_url: string;
+    image_low_url: string;
+  };
+  name: string;
+  last_name: string;
+  email: string;
+  role: {
+    value: number;
+    label: string;
+  };
+  last_login: Date;
+  staff_role: {
+    value: number;
+    label: string;
+  };
+};
